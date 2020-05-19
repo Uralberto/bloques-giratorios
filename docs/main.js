2fe238fd7e8fdf682bc6c6ad8d5d42a989456648
Uriel Arevalo <urievalo@hotmail.com>
2020-05-19 18:09:17 -0500

    Primer commit

---
+++ docs/main.js
@@ -0,0 +1,53 @@
+
+
+
+let preload = () => {
+    
+    
+    const blocks = document.querySelectorAll('.preloader .blocks .block')
+    let interval = 0
+    
+    blocks.forEach((block, i) => {
+        console.log(block, i)
+        
+        setTimeout(() => {
+            animate(block, i)
+        }, interval)
+
+        interval += 2000
+    })
+
+
+    function animate(block, i) {
+        let position = i
+       
+        setInterval(() => {
+            switch (position) {
+                case 0:
+                    block.style.top = "40px"
+                    position = 3
+                    break
+                case 1:
+                    block.style.left = "40px"
+                    position = 0
+                    break
+                case 2:
+                    block.style.top = "0px"
+                    position = 1
+                    break
+                case 3:
+                    block.style.left = "0px"
+                    position = 2
+                    break
+            }
+        },6000)
+        
+    }
+
+}
+
+
+
+preload()
+
+
